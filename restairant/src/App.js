bc94a082ba184e4126bf7b73cb424e8e9953775e
hamzahhisham306 <hamzahhisham306@gmail.com>
2022-12-25 08:33:31 +0000

    add header

--- restairant/src/App.js
+++ restairant/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from './Components/Navbar/Header/Header';
 import Navbar from './Components/Navbar/Nav/Navbar';
 
 function App() {
   return (
     <div>
     <Navbar/>
+    <Header/>
     </div>
   );
 }
